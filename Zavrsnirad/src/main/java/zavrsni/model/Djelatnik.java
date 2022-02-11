32676476df49692ba0937032c4e68a8dfbd94568
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-11 16:57:14 +0100

    Dodavanje početne start klase i klase za djelatnika

---
+++ Zavrsnirad/src/main/java/zavrsni/model/Djelatnik.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Igor
+ */
+@Entity
+public class Djelatnik {
+    @Id
+    private int sifra;
+    private String ime;
+    private String prezime;
+    private String uloga;
+    private String email;
+    private BigDecimal placa;
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getUloga() {
+        return uloga;
+    }
+
+    public void setUloga(String uloga) {
+        this.uloga = uloga;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public BigDecimal getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(BigDecimal placa) {
+        this.placa = placa;
+    }
+    
+    
+}
