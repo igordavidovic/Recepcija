dc7e668c9a84fbc5c69e56ee420dfab95effbecb
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-14 13:46:26 +0100

    Dodavanje klasa za posjetu i uslugu

---
+++ Zavrsnirad/src/main/java/zavrsni/model/Posjeta.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Igor
+ */
+@Entity
+public class Posjeta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer sifra;
+    private Korisnik korisnik;
+    @Column(name = "datumprijave")
+    private Date datumPrijave;
+    @Column(name = "datumodjave")
+    private Date datumOdjave;
+    @Column(name = "brojsoba")
+    private Integer brojSoba;
+    @Column(name = "brojodraslih")
+    private Integer brojOdraslih;
+    @Column(name = "brojdjece")
+    private Integer brojDjece;
+
+    public Integer getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(Integer sifra) {
+        this.sifra = sifra;
+    }
+    
+    
+    public Korisnik getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(Korisnik korisnik) {
+        this.korisnik = korisnik;
+    }
+    
+    public Date getDatumPrijave() {
+        return datumPrijave;
+    }
+
+    public void setDatumPrijave(Date datumPrijave) {
+        this.datumPrijave = datumPrijave;
+    }
+
+    public Date getDatumOdjave() {
+        return datumOdjave;
+    }
+
+    public void setDatumOdjave(Date datumOdjave) {
+        this.datumOdjave = datumOdjave;
+    }
+
+    public Integer getBrojSoba() {
+        return brojSoba;
+    }
+
+    public void setBrojSoba(Integer brojSoba) {
+        this.brojSoba = brojSoba;
+    }
+
+    public Integer getBrojOdraslih() {
+        return brojOdraslih;
+    }
+
+    public void setBrojOdraslih(Integer brojOdraslih) {
+        this.brojOdraslih = brojOdraslih;
+    }
+
+    public Integer getBrojDjece() {
+        return brojDjece;
+    }
+
+    public void setBrojDjece(Integer brojDjece) {
+        this.brojDjece = brojDjece;
+    }
+    
+}
---
+++ Zavrsnirad/src/main/java/zavrsni/model/Usluga.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Igor
+ */
+@Entity
+public class Usluga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int sifra;
+    private Djelatnik djelatnik;
+    @Column(columnDefinition = "varchar(100)")
+    private String naziv;
+    @Column(columnDefinition = "decimal(18,2)",nullable = true)
+    private BigDecimal cijena;
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public Djelatnik getDjelatnik() {
+        return djelatnik;
+    }
+
+    public void setDjelatnik(Djelatnik djelatnik) {
+        this.djelatnik = djelatnik;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public BigDecimal getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(BigDecimal cijena) {
+        this.cijena = cijena;
+    }
+    
+    
+}
