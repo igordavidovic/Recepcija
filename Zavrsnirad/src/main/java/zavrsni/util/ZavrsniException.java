0ac2f053f8127a39cb399803525b5fe68b709b32
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-24 19:14:11 +0100

    Dodavanje vlastite iznimke

---
+++ Zavrsnirad/src/main/java/zavrsni/util/ZavrsniException.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.util;
+
+/**
+ *
+ * @author Igor
+ */
+public class ZavrsniException extends Exception {
+
+    private String poruka;
+
+    public ZavrsniException(String poruka) {
+        super();
+        this.poruka = poruka;
+    }
+
+    public String getPoruka() {
+        return poruka;
+    }
+    
+    
+}
