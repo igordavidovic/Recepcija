8ba030172988240b05cd397493e70e24a71c4fba
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-02-25 17:04:08 +0100

    Pokušaj kontrole unosa imena i prezimena

---
+++ Zavrsnirad/src/main/java/zavrsni/controller/ObradaKorisnik.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.controller;
+
+import java.util.List;
+import zavrsni.model.Korisnik;
+import zavrsni.util.ZavrsniException;
+import zavrsni.util.ZavrsniUtil;
+
+/**
+ *
+ * @author Igor
+ */
+public class ObradaKorisnik extends Obrada<Korisnik> {
+
+    @Override
+    public List<Korisnik> read() {
+        return session.createQuery("from Korisnik").list();
+    }
+
+    @Override
+    protected void kontrolaCreate() throws ZavrsniException {
+        kontrolaIme();
+        kontrolaPrezime();
+    }
+
+    @Override
+    protected void kontrolaUpdate() throws ZavrsniException {
+        kontrolaIme();
+        kontrolaPrezime();
+    }
+
+    @Override
+    protected void kontrolaDelete() throws ZavrsniException {
+    }
+
+    private void kontrolaIme() throws ZavrsniException {
+        if (entitet.getIme() == null || entitet.getIme().trim().isEmpty()) {
+            throw new ZavrsniException("Ime mora biti uneseno");
+        }
+        if (entitet.getIme().length() > 50) {
+            throw new ZavrsniException("Ime ne smije sadržavati više od 50 znakova");
+        }
+        boolean b = ZavrsniUtil.provjeraZnakova(entitet.getIme());
+        if (b == false) {
+            throw new ZavrsniException("Ime ne smije sadržavati brojeve ili posebne znakove(#,$,%,& etc.)");
+        }
+
+    }
+
+    private void kontrolaPrezime() throws ZavrsniException {
+        if (entitet.getPrezime() == null || entitet.getPrezime().trim().isEmpty()) {
+            throw new ZavrsniException("Prezime mora biti uneseno");
+        }
+        if (entitet.getPrezime().length() > 50) {
+            throw new ZavrsniException("Prezime ne smije sadržavati više od 50 znakova");
+        }
+        boolean b = ZavrsniUtil.provjeraZnakova(entitet.getPrezime());
+        if (b == false) {
+            throw new ZavrsniException("Prezime ne smije sadržavati brojeve ili posebne znakove(#,$,%,& etc.)");
+        }
+
+    }
+}
---
+++ Zavrsnirad/src/main/java/zavrsni/util/ZavrsniUtil.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zavrsni.util;
+
+/**
+ *
+ * @author Igor
+ */
+public class ZavrsniUtil {
+
+    public static boolean provjeraZnakova(String s) {
+        char[] c = {'<', '>', '!', '~', '"', 'ˇ', '#', '^', '$', '˘', '%', '°', '&', '˛', '/', '`', '(', '˙', ')', '´', '=', '˝', '?', '¨', '*', '+', '¸', '\\', '|', '€', '÷', '×', '[', ']', 'ł', 'Ł', '¤', '@', '{', '}', '§', ',', ';', '.', ':', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        char[] p = s.toCharArray();
+        for(int i = 0;i < p.length;i++){
+            for(int j = 0;j < c.length;j++){
+                if(p[i] == c[j]){
+                 return false;   
+                }
+            }
+        }
+        return true;
+    }
+    
+}
