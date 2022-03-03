23339a93dba18c836ea90b4690d67587f7a8ba29
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-03-03 16:38:16 +0100

    Testiranje aplikacije

--- Zavrsnirad/src/main/java/zavrsni/Start.java
+++ Zavrsnirad/src/main/java/zavrsni/Start.java
@@ -4,14 +4,44 @@
  */
 package zavrsni;
 
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import java.math.BigDecimal;
+import org.hibernate.Session;
+import zavrsni.model.Djelatnik;
 import zavrsni.util.HibernateUtil;
+import zavrsni.view.SplashScreen;
 
 /**
  *
  * @author Igor
  */
 public class Start {
+
+    private Session s;
+    private Argon2 argon2;
+
+    public Start() {
+        //s = HibernateUtil.getSession();
+        //dodavanjeDjelatnika();
+        new SplashScreen().setVisible(true);
+    }
+
+    private void dodavanjeDjelatnika() {
+        s.beginTransaction();
+        Djelatnik d = new Djelatnik();
+        argon2 = Argon2Factory.create();
+        d.setIme("Igor");
+        d.setPrezime("Davidović");
+        d.setEmail("idavidovic@gmail.com");
+        d.setPlaca(new BigDecimal(5500));
+        d.setUloga("Recepcionar");
+        d.setLozinka(argon2.hash(10, 65536, 1, "id".toCharArray()));
+        s.save(d);
+        s.getTransaction().commit();
+    }
+
     public static void main(String[] args) {
-        HibernateUtil.getSession();
+        new Start();
     }
 }
