ae8371fcdf3e8cf944bf02aa37c0f29de36aca8f
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-03-03 16:37:52 +0100

    Dodavanje potrebnog za naslove u aplikaciji

--- Zavrsnirad/src/main/java/zavrsni/util/ZavrsniUtil.java
+++ Zavrsnirad/src/main/java/zavrsni/util/ZavrsniUtil.java
@@ -4,34 +4,45 @@
  */
 package zavrsni.util;
 
+import zavrsni.model.Djelatnik;
+
 /**
  *
  * @author Igor
  */
 public class ZavrsniUtil {
 
+    public static Djelatnik djelatnik;
+    public static final String APP_NASLOV = "Recepcija app";
+
+    public static String getNaslov(String naslov) {
+        if (ZavrsniUtil.djelatnik == null) {
+            return new StringBuilder().append(APP_NASLOV).append(" ").append(naslov).toString();
+        }
+        return new StringBuilder().append(APP_NASLOV).append(" ").append(naslov).append(" ").append(ZavrsniUtil.djelatnik.getIme()).append(" ").append(ZavrsniUtil.djelatnik.getPrezime()).append(" - ").append(ZavrsniUtil.djelatnik.getUloga()).toString();
+    }
+
     public static boolean provjeraZnakova(String s) {
         char[] c = {'<', '>', '!', '~', '"', 'ˇ', '#', '^', '$', '˘', '%', '°', '&', '˛', '/', '`', '(', '˙', ')', '´', '=', '˝', '?', '¨', '*', '+', '¸', '\\', '|', '€', '÷', '×', '[', ']', 'ł', 'Ł', '¤', '@', '{', '}', '§', ',', ';', '.', ':', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         char[] p = s.toCharArray();
-        for(int i = 0;i < p.length;i++){
-            for(int j = 0;j < c.length;j++){
-                if(p[i] == c[j]){
-                 return false;   
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < c.length; j++) {
+                if (p[i] == c[j]) {
+                    return false;
                 }
             }
         }
         return true;
     }
-    
-    
-    public static boolean provjeraUloge(String s){
-        String[] niz = {"RECEPCIONAR","POMOĆNI RECEPCIONAR","NOĆNI RECEPCIONAR","ŠEF RECEPCIJE","ZAMJENIK ŠEFA RECEPCIJE","PORTIR","BELL BOY"};
-        for(String a : niz){
-            if(s.toUpperCase().equals(a)){
+
+    public static boolean provjeraUloge(String s) {
+        String[] niz = {"RECEPCIONAR", "POMOĆNI RECEPCIONAR", "NOĆNI RECEPCIONAR", "ŠEF RECEPCIJE", "ZAMJENIK ŠEFA RECEPCIJE", "PORTIR", "BELL BOY"};
+        for (String a : niz) {
+            if (s.toUpperCase().equals(a)) {
                 return true;
             }
         }
-        
+
         return false;
     }
 }
