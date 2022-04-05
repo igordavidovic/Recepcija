554fa23c1cb0a2f94568689a3622de29c302f5f0
igordavidovic <93661596+igordavidovic@users.noreply.github.com>
2022-04-05 20:17:51 +0200

    Dodavanje kontrola posjete

--- Zavrsnirad/src/main/java/zavrsni/controller/ObradaPosjeta.java
+++ Zavrsnirad/src/main/java/zavrsni/controller/ObradaPosjeta.java
@@ -19,12 +19,19 @@ public class ObradaPosjeta extends Obrada<Posjeta> {
         return session.createQuery("from Posjeta").list();
     }
 
+    public List<Posjeta> read(String uvjet) {
+        return session.createQuery("from Posjeta p "
+                + " where concat(p.datumPrijave,' ',p.datumOdjave) "
+                + " like :uvjet order by p.datumPrijave, p.datumOdjave").setParameter("uvjet", "%" + uvjet + "%").list();
+    }
+
     @Override
     protected void kontrolaCreate() throws ZavrsniException {
         kontrolaKorisnik();
         kontrolaBrojSoba();
         kontrolaBrojOdraslih();
         kontrolaBrojDjece();
+        kontrolaUsluga();
     }
 
     @Override
@@ -33,6 +40,7 @@ public class ObradaPosjeta extends Obrada<Posjeta> {
         kontrolaBrojSoba();
         kontrolaBrojOdraslih();
         kontrolaBrojDjece();
+        kontrolaUsluga();
     }
 
     @Override
@@ -40,36 +48,41 @@ public class ObradaPosjeta extends Obrada<Posjeta> {
     }
 
     private void kontrolaKorisnik() throws ZavrsniException {
-        if(entitet.getKorisnik() == null || entitet.getKorisnik().getSifra() == 0){
+        if (entitet.getKorisnik() == null || entitet.getKorisnik().getSifra() == 0) {
             throw new ZavrsniException("Korisnik mora biti postavljen na posjeti");
         }
     }
 
     private void kontrolaBrojSoba() throws ZavrsniException {
-        if(entitet.getBrojSoba() == null){
+        if (entitet.getBrojSoba() == null) {
             throw new ZavrsniException("Broj soba mora biti unesen");
         }
-        if(entitet.getBrojSoba() > 10 || entitet.getBrojSoba() < 1){
+        if (entitet.getBrojSoba() > 10 || entitet.getBrojSoba() < 1) {
             throw new ZavrsniException("Broj soba može biti između 1 i 10");
         }
     }
 
     private void kontrolaBrojOdraslih() throws ZavrsniException {
-         if(entitet.getBrojOdraslih() == null){
+        if (entitet.getBrojOdraslih() == null) {
             throw new ZavrsniException("Broj odraslih mora biti unesen");
         }
-        if(entitet.getBrojOdraslih() > 20 || entitet.getBrojOdraslih()< 1){
+        if (entitet.getBrojOdraslih() > 20 || entitet.getBrojOdraslih() < 1) {
             throw new ZavrsniException("Broj odraslih može biti između 1 i 20");
         }
     }
 
     private void kontrolaBrojDjece() throws ZavrsniException {
-         if(entitet.getBrojDjece() == null){
+        if (entitet.getBrojDjece() == null) {
             throw new ZavrsniException("Broj djece mora biti unesen");
         }
-        if(entitet.getBrojDjece() > 20 || entitet.getBrojSoba() < 0){
+        if (entitet.getBrojDjece() > 20 || entitet.getBrojSoba() < 0) {
             throw new ZavrsniException("Broj djece može biti između 0 i 20");
         }
     }
 
+    private void kontrolaUsluga() throws ZavrsniException {
+        if (entitet.getUsluge() == null || entitet.getUsluge().isEmpty()) {
+            throw new ZavrsniException("Posjeta mora sadržati usluge");
+        }
+    }
 }
